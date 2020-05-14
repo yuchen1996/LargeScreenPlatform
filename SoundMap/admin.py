1731d66a697ee375c675b1036ce5b2db4d848556
YuChen <yuchen@dmser.com.cn>
2020-05-14 16:40:49 +0800

    Add Video Model.

--- SoundMap/admin.py
+++ SoundMap/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import SoundMap, Audio, Image
+from .models import SoundMap, Audio, Image, Video
 
 
 class AudioInline(admin.TabularInline):
@@ -14,6 +14,23 @@ class AudioInline(admin.TabularInline):
         ),
         (
             '内容信息', {'fields': ['pause_image', 'play_image', 'sound_file', 'sound_length']}
+        ),
+
+    ]
+    extra = 0
+
+
+class VideoInline(admin.TabularInline):
+    model = Video
+    fieldsets = [
+        (
+            '名称信息', {'fields': ['name', 'is_show_name', 'name_size', 'name_color', 'name_loc']}
+        ),
+        (
+            '位置信息', {'fields': ['loc_left', 'loc_top', 'size_width', 'size_height', 'is_show']}
+        ),
+        (
+            '内容信息', {'fields': ['pause_image', 'play_image', 'video_file', 'video_length']}
         ),
 
     ]
@@ -41,7 +58,7 @@ class SoundMapAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['name', 'background_image']})
     ]
-    inlines = [AudioInline, ImageInline]
+    inlines = [AudioInline, VideoInline, ImageInline]
     list_display = ('name', 'background_image', 'bak_1', 'bak_2', 'bak_3')
     search_fields = ['name']
 
--- SoundMap/models.py
+++ SoundMap/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+LOCATION_CHOICES = (
+        (1, '上'),
+        (2, '下'),
+        (3, '左'),
+        (4, '右'),
+    )
+
+
 # 声音地图主表
 class SoundMap(models.Model):
     name = models.CharField(max_length=20, verbose_name='项目名称')
@@ -15,12 +23,6 @@ class SoundMap(models.Model):
 
 # 声音地图声音明细表
 class Audio(models.Model):
-    LOCATION_CHOICES = (
-        (1, '上'),
-        (2, '下'),
-        (3, '左'),
-        (4, '右'),
-    )
     map = models.ForeignKey(SoundMap, on_delete=models.CASCADE)
     name = models.CharField(max_length=20, verbose_name='素材名称')
     is_show_name = models.BooleanField(verbose_name='是否显示名称', default=True)
@@ -59,4 +61,28 @@ class Image(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# 声音地图视频明细表
+class Video(models.Model):
+    map = models.ForeignKey(SoundMap, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20, verbose_name='素材名称')
+    is_show_name = models.BooleanField(verbose_name='是否显示名称', default=True)
+    name_size = models.CharField(max_length=20, verbose_name='名称字体大小', blank=True, null=True)
+    name_color = models.CharField(max_length=20, verbose_name='名称字体颜色', blank=True, null=True, default='#000')
+    name_loc = models.IntegerField(verbose_name='名称位置', choices=LOCATION_CHOICES)
+    loc_left = models.CharField(max_length=20, verbose_name='位置-左')
+    loc_top = models.CharField(max_length=20, verbose_name='位置-上')
+    size_width = models.CharField(max_length=20, verbose_name='图标宽度', blank=True, null=True)
+    size_height = models.CharField(max_length=20, verbose_name='图标高度', blank=True, null=True)
+    pause_image = models.ImageField(verbose_name='静止时显示图片', width_field='size_width', height_field='size_height')
+    play_image = models.ImageField(verbose_name='播放时显示图片')
+    video_file = models.FileField(verbose_name='视频文件')
+    video_length = models.IntegerField(verbose_name='播放时长', blank=True, null=True)
+    is_show = models.BooleanField(verbose_name='是否显示', default=True)
+
+    def __str__(self):
+        return self.name
+
+
 
